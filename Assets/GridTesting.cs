d19c09fb6c3bea5fa5672fb74bb43f585b8a7cf8
Tom <60851185+TJJonker@users.noreply.github.com>
2022-04-29 14:45:49 +0200

    Update on grid system

--- Assets/GridTesting.cs
+++ Assets/GridTesting.cs
@@ -1,10 +1,22 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using Grid = Jonko.Grids.Grid;
+using Jonko.Utils;
 
 public class GridTesting : MonoBehaviour
 {
+    private Grid grid;
+
     private void Start()
     {
-        Grid grid = new Grid(20, 10, .5f);
+        grid = new Grid(20, 10, .5f);
+    }
+
+    private void Update()
+    {
+        if (Mouse.current.leftButton.IsPressed())
+        {
+            grid.SetValue(Utils.MouseToScreen(Mouse.current.position.ReadValue()), 69);
+        }
     }
 }
--- Assets/Scripts/Jonko/Grid.cs
+++ Assets/Scripts/Jonko/Grid.cs
@@ -24,7 +24,7 @@ namespace Jonko.Grids {
                 for(int y = 0; y < gridArray.GetLength(1); y++)
                 {
                     debugTextArray[x, y] = Visualisation.Visualisation.CreateWorldText(gridArray[x,y].ToString(), Color.white, null, 
-                        GetWorldPosition(x, y) + Vector3.one * cellSize * .5f, 4, TextAnchor.MiddleCenter, TextAlignment.Center);
+                        GetWorldPosition(x, y) + Vector2.one * cellSize * .5f, 4, TextAnchor.MiddleCenter, TextAlignment.Center);
                     Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100);
                     Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100);
                 }
@@ -35,8 +35,51 @@ namespace Jonko.Grids {
             SetValue(4, 7, 69);
         }
 
-        private Vector3 GetWorldPosition(int x, int y) => new Vector3(x, y) * cellSize;
+        /// <summary>
+        ///     Converts grid position to world position
+        /// </summary>
+        /// <param name="position"> Vector2 grid position </param>
+        /// <returns></returns>
+        private Vector2 GetWorldPosition(Vector2 position)
+            => position * cellSize;
 
+        /// <summary>
+        ///     Converts grid position to world position
+        /// </summary>
+        /// <param name="x"> X position on the grid </param>
+        /// <param name="y"> Y position on the grid </param>
+        /// <returns></returns>
+        private Vector2 GetWorldPosition(int x, int y) 
+            => GetWorldPosition(new Vector2(x, y));
+
+        /// <summary>
+        ///     Converts world position to grid position
+        /// </summary>
+        /// <param name="position"> World position </param>
+        /// <param name="x"> X position on the grid </param>
+        /// <param name="y"> Y position on the grid </param>
+        private void GetGridPosition(Vector2 position, out int x, out int y)
+        {
+            x = Mathf.FloorToInt(position.x / cellSize);
+            y = Mathf.FloorToInt(position.y / cellSize);
+        }
+
+        /// <summary>
+        ///     Converts world position to grid position
+        /// </summary>
+        /// <param name="posX"> X position in the world </param>
+        /// <param name="posY"> Y position in the world </param>
+        /// <param name="x"> X position on the grid </param>
+        /// <param name="y"> Y position on the grid </param>
+        private void GetGridPosition(int posX, int posY, out int x, out int y)
+            => GetGridPosition(new Vector2(posX, posY), out x, out y);
+
+        /// <summary>
+        ///     Sets the value a specific place in the grid
+        /// </summary>
+        /// <param name="x"> X position on the grid </param>
+        /// <param name="y"> Y position on the grid </param>
+        /// <param name="value"> Value to put in the grid spot </param>
         public void SetValue(int x, int y, int value)
         {
             if(x >= 0 && y >= 0 && x < width && y < height)
@@ -45,5 +88,19 @@ namespace Jonko.Grids {
                 debugTextArray[x,y].text = gridArray[x, y].ToString();
             }
         }
+
+        /// <summary>
+        ///     Sets the value a specific place in the grid
+        /// </summary>
+        /// <param name="position"> Position on the grid </param>
+        /// <param name="value"> Value to put in the grid spot </param>
+        public void SetValue(Vector3 position, int value)
+        {
+            int x, y;
+            GetGridPosition(position, out x, out y);
+            SetValue(x, y, value);
+        }
+
+
     }
 }
